bd9fc3029d6d65bce91669c0599199d71dc7f722
yukis1996 <58467980+yukis1996@users.noreply.github.com>
2020-06-08 23:49:16 +0900

    recipe_search
    or検索and検索対応

--- lecture2/demo/src/main/resources/META-INF/jp/co/aivick/demo/dao/RecipeDao/search.sql
+++ lecture2/demo/src/main/resources/META-INF/jp/co/aivick/demo/dao/RecipeDao/search.sql
@@ -1,5 +1,24 @@
 select
     /*%expand*/*
 from recipes
-where name
+where
+/*%if beforeCal == null && afterCal == null*/
+AND name
+	Like /* "%" + search "%" + */'a'
+/*%elseif search != null && beforeCal != null && afterCal != null*/	
+	AND name
+	Like /* "%" + search "%" + */'a'
+	AND
+	/*afterCal*/0 >= cal
+	AND 
+	/*beforeCal*/0 <= cal	
+/*%elseif beforeCal == null*/
+	AND
+	/*afterCal*/0 >= cal	
+/*%elseif afterCal == null*/
+	AND 
+	/*beforeCal*/0 <= cal
+/*%end*/
+AND name
 Like /* "%" + search "%" + */'a'
+
